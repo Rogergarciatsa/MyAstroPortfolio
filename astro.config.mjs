797bea71f6cb264f06d011442da078a7d360cc17
Roger Garcia <96830104+Rogergarciatsa@users.noreply.github.com>
2024-01-02 13:17:42 +0000

    insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import { injectSpeedInsights } from "@vercel/speed-insights"
+
 
 import tailwind from "@astrojs/tailwind";
-
+import { injectSpeedInsights } from "@vercel/speed-insights"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
